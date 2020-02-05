684f27a70588027acd334b47ac909ad622ee2363
lliuti <dev.lliuti@gmail.com>
2020-02-05 00:08:00 -0300

    Adding TopTabNavigator

---
+++ src/pages/Status/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Status() {
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
 
 import Chats from './pages/Chats/index';
+import Status from './pages/Status/index';
 
-const Routes = createAppContainer(createSwitchNavigator({
+const Routes = createAppContainer(createMaterialTopTabNavigator({
   Chats,
+  Status
+},{
+  tabBarOptions: {
+
+  },
+  defaultNavigationOptions: {
+    tabBarVisible: false,
+  },
 }));
 
 export default Routes;
