c193fe1e519dee3e2bff260cdfcb8f5dd43e5937
lliuti <dev.lliuti@gmail.com>
2020-02-05 22:37:06 -0300

    Creating status page

--- src/pages/Status/index.js
+++ src/pages/Status/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons/'
 
-import { Container } from './styles';
+import { 
+  Container,
+  MyStatusBox,
+  ImageBox,
+  Image,
+  AddStatusBox,
+  Details,
+  Top,
+  TitleBox,
+  Title,
+  UpdateStatus,
+  Text
+} from './styles';
 
 export default function Status() {
   return (
     <Container>
-      
+      <MyStatusBox>
+        <ImageBox>
+          <Image style={{ width: 50, height: 50, borderRadius: 50 }} source={{ uri: 'https://avatars3.githubusercontent.com/u/53618919?s=400&u=1685efc02d9f5f2a1e470efc7ce7e745e665967e&v=4' }}/>
+          <AddStatusBox>
+            <MaterialIcons name='add' size={18} color='#fff'/>
+          </AddStatusBox>
+        </ImageBox>
+        <Details>
+          <Top>
+            <TitleBox>
+              <Title>Meu status</Title>
+            </TitleBox>
+          </Top>
+          <UpdateStatus>
+            <Text>Toque para atualizar seu status</Text>
+          </UpdateStatus>
+        </Details>
+      </MyStatusBox>
     </Container>
   );
 }
--- src/pages/Status/styles.js
+++ src/pages/Status/styles.js
@@ -1,5 +1,57 @@
+import { StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  
+  flex: 1;
+  background: #fff;
+`;
+
+export const MyStatusBox = styled.View`
+  flex-direction: row;
+  /* border-bottom-color: #ccc;
+  border-bottom-width: ${StyleSheet.hairlineWidth}px; */
+`;
+
+export const ImageBox = styled.View`
+  padding: 10px 10px;
+`;
+
+export const Image = styled.Image``;
+
+export const AddStatusBox = styled.View`
+  background: #25D366;
+  width: 20px;
+  height: 20px;
+  border-radius: 50;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 5px;
+  right: 10px;
+`;
+
+export const Details = styled.View`
+  flex: 1;
+  padding: 0 10px;
+  margin-right: 10px;
+  justify-content: center;
+`;
+
+export const Top = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TitleBox = styled.View``;
+
+export const Title = styled.Text`
+  font-size: 18px;
+`;
+
+export const UpdateStatus = styled.View``;
+
+export const Text = styled.Text`
+  color: #666;
 `;
